5f84c2281f6e210c652e4e2b6da0cd8a033ea4fe
GruBaXu <108203631+LTGBLSN@users.noreply.github.com>
2025-09-30 19:17:38 +0800

    init，源于步兵

---
+++ code_mf/Src/uart_sent.c
@@ -0,0 +1,64 @@
+//
+// Created by 21481 on 2025/3/16.
+//
+
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+#include "cmsis_os.h"
+#include "dma.h"
+#include "usart.h"
+#include "gpio.h"
+#include "uart_printf.h"
+#include "uart_sent.h"
+#include "bsp_rc.h"
+#include "remote_control.h"
+#include "CAN_receive.h"
+#include "jy61p.h"
+#include "chassis_motor_control.h"
+#include "auto_aim.h"
+
+
+
+
+
+
+void uart_sent_debug()
+{
+    while (1)
+    {
+        usart6_printf("%d,%d,%f,%f,%f,%f,%f \r\n",
+                      rc_s1,
+                      auto_aim_rx_packet.tracking,
+                      pitch_radian_from_bmi088,
+                      yaw_angle_from_bmi088,
+                      auto_aim_rx_packet.y,
+                      auto_aim_rx_packet.x,
+                      infantry_auto_aim_target.yaw_angle);
+
+
+
+        osDelay(5);
+
+
+
+
+    }
+
+}
+
+
+void aim_uart_sent()
+{
+    while (1)
+    {
+        sent_data_update();
+
+        osDelay(5);
+        osDelay(1);
+    }
+}
+
+
+
+
